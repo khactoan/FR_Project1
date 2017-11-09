7373f3df0ab840214664785ca160d97e9418f5ce
ToanDK <khactoan96@gmail.com>
2017-11-09 09:32:27 +0700

    Aplly theme metronic

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107082643) do
+ActiveRecord::Schema.define(version: 20171109005857) do
 
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
-    t.string "content"
-    t.integer "user_id"
+    t.text "content"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "relationships", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -36,4 +37,5 @@ ActiveRecord::Schema.define(version: 20171107082643) do
     t.string "remember_digest"
   end
 
+  add_foreign_key "posts", "users"
 end
